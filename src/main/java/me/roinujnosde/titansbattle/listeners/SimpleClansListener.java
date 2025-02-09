20beabd4a5a94e9c4e72640cd1eff7f74077152e
ThiagoROX <51332006+SrBedrock@users.noreply.github.com>
2025-02-09 18:40:08 -0300

    Add event handling for player leaving and kicking from clans in group mode

--- src/main/java/me/roinujnosde/titansbattle/listeners/SimpleClansListener.java
+++ src/main/java/me/roinujnosde/titansbattle/listeners/SimpleClansListener.java
@@ -6,6 +6,7 @@ import me.roinujnosde.titansbattle.types.Group;
 import me.roinujnosde.titansbattle.types.Warrior;
 import net.sacredlabyrinth.phaed.simpleclans.events.PreDisbandClanEvent;
 import net.sacredlabyrinth.phaed.simpleclans.events.PrePlayerKickedClanEvent;
+import net.sacredlabyrinth.phaed.simpleclans.events.PrePlayerLeaveClanEvent;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 
@@ -23,11 +24,32 @@ public class SimpleClansListener extends TBListener {
 
     @EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
     public void onPrePlayerKickedClan(PrePlayerKickedClanEvent event) {
+        plugin.getLogger().info("PrePlayerKickedClanEvent");
         gm.getCurrentGame().ifPresent(game -> {
             if (game.getConfig().isGroupMode()) {
+                plugin.getLogger().info("Group mode");
                 List<UUID> participants = game.getParticipants().stream().map(Warrior::getUniqueId).toList();
                 UUID target = event.getClanPlayer().getUniqueId();
+                plugin.getLogger().info("Target: " + target);
                 if (participants.contains(target)) {
+                    plugin.getLogger().info("Player is in a group event");
+                    event.setCancelled(true);
+                }
+            }
+        });
+    }
+
+    @EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
+    public void onPrePlayerLeaveClan(PrePlayerLeaveClanEvent event) {
+        plugin.getLogger().info("PrePlayerLeaveClanEvent");
+        gm.getCurrentGame().ifPresent(game -> {
+            if (game.getConfig().isGroupMode()) {
+                plugin.getLogger().info("Group mode");
+                List<UUID> participants = game.getParticipants().stream().map(Warrior::getUniqueId).toList();
+                UUID target = event.getPlayer().getUniqueId();
+                plugin.getLogger().info("Target: " + target);
+                if (participants.contains(target)) {
+                    plugin.getLogger().info("Player is in a group event");
                     event.setCancelled(true);
                 }
             }
@@ -36,11 +58,15 @@ public class SimpleClansListener extends TBListener {
 
     @EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
     public void onPreDisbandClan(PreDisbandClanEvent event) {
+        plugin.getLogger().info("PreDisbandClanEvent");
         gm.getCurrentGame().ifPresent(game -> {
             if (game.getConfig().isGroupMode()) {
+                plugin.getLogger().info("Group mode");
                 String clanTag = event.getClan().getTag();
+                plugin.getLogger().info("Clan tag: " + clanTag);
                 for (Map.Entry<Group, Integer> entry : game.getGroupParticipants().entrySet()) {
                     if (entry.getKey().getId().equalsIgnoreCase(clanTag)) {
+                        plugin.getLogger().info("Clan is in a group event");
                         event.setCancelled(true);
                         break;
                     }
